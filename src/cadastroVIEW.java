3f1409d884d617572ae4ac8cf2e0473d1cf6bef3
“Edvam <turcattoe@gmail.com>
2024-08-19 14:02:51 -0300

    Botão de Salvar produto

--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -148,9 +151,15 @@ public class cadastroVIEW extends javax.swing.JFrame {
         produto.setValor(Integer.parseInt(valor));
         produto.setStatus(status);
         
-        ProdutosDAO produtodao = new ProdutosDAO();
-        produtodao.cadastrarProduto(produto);
+        ProdutosDAO produtodao = new ProdutosDAO();   
+        boolean sucesso = produtodao.cadastrarProduto(produto);
         
+         if (sucesso) {
+        JOptionPane.showMessageDialog(this, "O Produto foi cadastrado com sucesso!");
+        } else {
+        JOptionPane.showMessageDialog(this, "Erro ao cadastrar este produto.");
+        }
+                
     }//GEN-LAST:event_btnCadastrarActionPerformed
 
     private void btnProdutosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProdutosActionPerformed
